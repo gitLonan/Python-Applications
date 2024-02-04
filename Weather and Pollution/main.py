18590b15aa70cea8daa4bad3680d701557c7b9d4
Lazar <sc2lazar@gmail.com>
2024-02-04 16:26:08 +0100

    layout of the window

--- Weather and Pollution/main.py
+++ Weather and Pollution/main.py
@@ -3,7 +3,7 @@ import json, urllib.request, time, sys, datetime, calendar
 from style_class import Style
 from user_class import User
 from network_class import Network_communication
-import PySimpleGUIQt as sg
+import PySimpleGUI as sg
 
 weather_data = {
         "City name": "No information",
@@ -76,15 +76,91 @@ def loading_pollution():
 
         print(pollution_data)
 
+def window_Current_weather_creation(win_location):
+    """
+    
+    """
+    sg.theme('LightBrown6')
+    BG_COLOR = sg.theme_text_color()
+    TXT_COLOR = sg.theme_background_color()
+    DEGREE_SIGN = u"\N{DEGREE SIGN}"
 
-def main(user):
-    latitude, longitude = Network_communication.get_latitude_longitude(user)
+    ################ THESE ARE KEYS FOR ALL THE THINGS #################
+    # top_col1 -> -data time-
+    # top_col2 -> -city name-
+    # top_layer -> -top layer-
+
+    # middle_col1 -> -image-
+    # middle_col2 -> -weather description-
+    # middle_col3 -> in fun metric_row -> -data_in_weatherDic- (name of the given metric form weather_data)
+    # middle_layer -> -middle layer-
+
+    #Gabriola
+    top_col1 = sg.Column([[sg.Text("Date time",background_color=BG_COLOR,text_color=TXT_COLOR, key="-data time-")]],element_justification='left',pad=(10, 5), expand_x=True,background_color=BG_COLOR)
+    top_col2 = sg.Column([[sg.Text(weather_data['City name'], background_color=BG_COLOR, text_color=TXT_COLOR, font=('Arial', 25), justification="left", key='-city name-')]],pad=(10, 5), expand_x=True,background_color=BG_COLOR)
+
+    top_layer = sg.Column([[top_col1, top_col2]],
+                                pad=(0,0),background_color=BG_COLOR,expand_y=True, expand_x=True, key="-top layer-")
+    
+    middle_col1 = sg.Column([[sg.Image("Weather and Pollution/sunny.PNG", size=(150,100),background_color=BG_COLOR, key="-image-")],
+                             [sg.Text(f"Weather: {weather_data['description']}",background_color=BG_COLOR,text_color=TXT_COLOR,pad=(10,0), key="-weather description-")]],background_color=BG_COLOR)
+    middle_col2 = sg.Column([[sg.Text(f'{weather_data["temp"]}{DEGREE_SIGN}C', font=('Haettenschweiler', 60),background_color=BG_COLOR, text_color=TXT_COLOR)]])
+    middle_col3 = sg.Column([metric_row('feels_like',DEGREE_SIGN,"Feels like:"), metric_row('temp_min',DEGREE_SIGN,"Area temp min:"), metric_row('temp_max',DEGREE_SIGN,"Area temp max:"), metric_row('pressure',"Pa","Pressure:"), metric_row('humidity',"%", "Humidity:"), metric_row('visibility',"m","Visibility:")],
+                        pad=((10, 0), (5, 10)), key='RtCOL')
+    
+    middle_layer = sg.Column([[middle_col1, middle_col2, middle_col3]],
+                                pad=(0,0),background_color=BG_COLOR, key="-middle layer-")
+
+    bottom_col1 = sg.Column([[sg.Text("TEST COL 3", font=('Arial', 8), background_color=BG_COLOR, text_color=TXT_COLOR, key='Updated')]],
+                      pad=(10, 5), element_justification='left', background_color=BG_COLOR, key='COL3', expand_x=True)
+
+    
+
+    
+
+    layout = [[top_layer],
+          [middle_layer],
+          
+          ]
+    windows = sg.Window(layout=layout,title="Weather",
+                        element_justification='center',
+                        margins=(0, 0),
+                        grab_anywhere=True,
+                        alpha_channel=0.8,
+                        right_click_menu=[[''], ['Current Weather', "Current Pollution", "Forecast Weather", "Forecast Pollution", 'Exit',]],
+                        no_titlebar=True,
+                        finalize=True,
+                        location=win_location)
+    
+    return windows
+
+
+def metric_row(data_in_weatherDic, symbol,text):
+    """ Return a pair of labels for each metric """
+
+    return [sg.Text(text, font=('Arial', 10), pad=(2, 0), size=(12, 1)),
+            sg.Text(f"{weather_data[data_in_weatherDic]}{symbol}", font=('Arial', 10, 'bold'), pad=(0, 0), size=(6, 1), key=data_in_weatherDic)]
+
+def main(user,win_location):
+    
+    #latitude, longitude = Network_communication.get_latitude_longitude(user)
     #Network_communication.get_current_weather_data(latitude, longitude, user)
     #Network_communication.get_current_pollution(latitude, longitude, user)
     #Network_communication.get_forecast_weatherFor_5days_data(latitude, longitude, user)
     #Network_communication.get_forcast_air_polution(latitude, longitude, user)
+    loading_current_weather(user)
+    loading_pollution()
 
+    
+    windows = window_Current_weather_creation(win_location)
 
+    while True:
+        event, values = windows.read()
+    
+        if event == "Exit":
+            sg.user_settings_set_entry('-win location-', windows.current_location())
+            break
+        
 
 
 if __name__ == "__main__":
@@ -92,9 +168,9 @@ if __name__ == "__main__":
     user.set_place_name()
     user.set_Alpha2_code_for_country()
     user.set_API_key()
-    loading_current_weather(user)
-    loading_pollution()
-    main(user)
+    win_location = sg.user_settings_get_entry('-win location-', (None, None))
+    
+    main(user,win_location)
 
 
 
